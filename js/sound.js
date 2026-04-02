3fd1af99d97d02dfd1cbe2e865c6bcd1ca4ba694
Sergey-Chernushevich <sergeychernushevich@gmail.com>
2026-04-02 18:47:41 +0300

    fix path

--- js/sound.js
+++ js/sound.js
@@ -1,13 +1,15 @@
+const base = new URL("../assets/sounds/", import.meta.url).href;
+
 export const sounds = {
-  hit:   new Audio("../assets/sounds/hit.mp3"),
-  brick: new Audio("../assets/sounds/brick.mp3"),
-  power: new Audio("../assets/sounds/power.mp3"),
-  win:   new Audio("../assets/sounds/sound_win.wav"),
-  lose:  new Audio("../assets/sounds/sound_lose.wav"),
+  hit: new Audio(`${base}hit.mp3`),
+  brick: new Audio(`${base}brick.mp3`),
+  power: new Audio(`${base}power.mp3`),
+  win: new Audio(`${base}sound_win.wav`),
+  lose: new Audio(`${base}sound_lose.wav`),
 };
 
-export const bgMusic    = new Audio("../assets/sounds/music_bg.mp3");
-bgMusic.loop   = true;
+export const bgMusic = new Audio(`${base}music_bg.mp3`);
+bgMusic.loop = true;
 bgMusic.volume = 0.35;
 
 export function startMusic() {
